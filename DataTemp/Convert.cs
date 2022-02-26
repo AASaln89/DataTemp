b693e9ddb71279da368e19399ac3b7a08668675c
Andrey Salnikov <kvanta13@yandex.ru>
2022-02-27 02:44:59 +0300

    123

---
+++ DataTemp/Convert.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace DataTemp
+{
+    class Convert : IValueConverter
+    {
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+
+        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((pCategories)value == pCategories.Food)
+            {
+                return "Еда";
+            }
+            return "Техника";
+        }
+    }
+}
